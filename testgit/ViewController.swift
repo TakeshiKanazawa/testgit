5afc9ed7de0dc5b2dee3e1762ea80a5d3b368ffa
Takeshi Kanazawa <oseans@hotmail.co.jp>
2020-04-22 23:13:25 +0900

    first

---
+++ testgit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testgit
+//
+//  Created by 金澤武士 on 2020/04/22.
+//  Copyright © 2020 tk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
